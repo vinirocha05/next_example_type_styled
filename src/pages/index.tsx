061ea1ae1592e04e9a3e26fca770c8cb02b2921a
Vinicius Rocha <vinirocha05@gmail.com>
2022-12-24 10:52:09 -0300

    adicionando header e estilo global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import Header from '../Components/Header';
+
 export default function Home() {
-  return <h1>olá</h1>;
+  return (
+    <>
+      <Header />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+        aperiam odio totam suscipit eligendi quod minima dolorem, quibusdam
+        numquam nam optio alias quis qui officia? Fugit animi necessitatibus
+        officiis?
+      </p>
+    </>
+  );
 }
